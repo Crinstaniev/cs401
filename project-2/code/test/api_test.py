d4640794ab86f3326fd6bb62cb48207df768eb0a
crinstaniev <zhuangzesen@gmail.com>
2023-02-21 14:05:33 -0500

    use pytest

--- project-2/code/test/api_test.py
+++ project-2/code/test/api_test.py
@@ -3,21 +3,28 @@ from pprint import pprint
 
 import pandas as pd
 import requests
-
-CLUSTER_IP = '10.106.208.198'
+import os
+import subprocess
 
 
 class TestAPI(unittest.TestCase):
     def test_api(self):
+        command = "kubectl get service project2-service-zz229 -n zz229 -o jsonpath='{.spec.clusterIP}'"
+        output = subprocess.check_output(command, shell=True)
+        CLUSTER_IP = output.decode("utf-8").strip()
         url = 'http://{}:30510/api/health'.format(CLUSTER_IP)
         response = requests.get(url, timeout=5)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {'status': 'ok'})
 
     def test_recommend(self):
+        command = "kubectl get service project2-service-zz229 -n zz229 -o jsonpath='{.spec.clusterIP}'"
+        output = subprocess.check_output(command, shell=True)
+        CLUSTER_IP = output.decode("utf-8").strip()
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
         url = 'http://{}:30510/api/recommend'.format(CLUSTER_IP)
         songs_list = pd.read_csv(
-            '../data/songs.csv')['track_name'].unique().tolist()
+            '../../data/songs.csv')['track_name'].unique().tolist()
         payload = {
             "songs": songs_list
         }
