0d138a7629c96ac37cd044264de9309a51f66d19
crinstaniev <zhuangzesen@gmail.com>
2023-03-04 03:35:51 -0500

    live update and cpu speedmeter

--- project-3/dashboard/app.py
+++ project-3/dashboard/app.py
@@ -1,81 +1,37 @@
-import json
-import threading
-import time
-from pprint import pprint
-
 import dash
 import dash_bootstrap_components as dbc
 import pandas as pd
 import redis
 from dash import dcc, html
-from dash_bootstrap_templates import load_figure_template
 from utils.helper_functions import fetch_data_from_redis
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__)
 r = redis.Redis(host='67.159.94.11', port=6379, db=0)
-
-load_figure_template('bootstrap')
 
 cpu_percent_history = None
 cpu_percent_ma = None
+cpu_freq = None
 
 
 def fetch():
-    global r, cpu_percent_history, cpu_percent_ma
+    global r, cpu_percent_history, cpu_percent_ma, cpu_freq
     data = fetch_data_from_redis(r)
 
     cpu_percent_history = pd.DataFrame(data['cpu_percent_history'], columns=[
         'cpu-{}'.format(i) for i in range(len(data['cpu_percent_history'][0]))])
     cpu_percent_ma = pd.DataFrame(data['cpu_percent_ma'], columns=[
         'cpu-{}'.format(i) for i in range(len(data['cpu_percent_ma'][0]))])
+    cpu_freq = data['cpu_freq_current']
 
-    time.sleep(4)
-
-
-def fetch_task():
-    while 1:
-        fetch()
-
-
-fetch()
-task = threading.Thread(target=fetch_task)
-task.start()
-
-# make cpu graphs
-cpu_graphs = []
-for i in range(len(cpu_percent_history.columns)):
-    cpu_graphs.append(
-        dbc.Col(
-            dcc.Graph(
-                id='cpu-{}'.format(i),
-                figure={
-                    'data': [
-                        {'x': cpu_percent_history.index, 'y': cpu_percent_history['cpu-{}'.format(
-                            i)], 'type': 'line', 'name': 'Usage'},
-                        {'x': cpu_percent_ma.index, 'y': cpu_percent_ma['cpu-{}'.format(
-                            i)], 'type': 'line', 'name': 'MA'},
-                    ],
-                    'layout': {
-                        'title': 'CPU-{}'.format(i),
-                        'height': 250,
-                        'xaxis': {'title': 'Time'},
-                        'yaxis': {'title': 'percentage'},
-                        'legend': {'orientation': 'h', 'yanchor': 'top'},
-                        'margin': {'t': 30, 'b': 30, 'r': 30, 'l': 30},
-                    },
-                }
-            )
-        )
-    )
-
-
-# create a 3 rows, 5 columns grid from cpu_graphs, with no spacing between columns and rows.
-cpu_graphs = [dbc.Row(cpu_graphs[0:5], className='g-4'),
-              dbc.Row(cpu_graphs[5:10], className='g-4'),
-              dbc.Row(cpu_graphs[10:15], className='g-4')]
 
 app.layout = dbc.Container(
     [
+        dcc.Interval(
+            id='interval-component',
+            # interval=5*1000,  # in milliseconds
+        ),
         dbc.Row(
             [
                 dbc.Col(
@@ -90,12 +46,93 @@ app.layout = dbc.Container(
                 )
             ]
         ),
-        dbc.Row([
-            dbc.Container(cpu_graphs)
-        ]),
+        dbc.Row(id='live-update-cpu-component'),
+        dbc.Row([dbc.Col(html.H3("CPU Frequency", className="text-header"))]),
+        dbc.Row(
+            dcc.Graph(id='cpu-speed-meter', animate=True)
+        )
     ],
     fluid=True,
 )
+
+
+@app.callback(
+    Output('live-update-cpu-component', 'children'),
+    Input('interval-component', 'n_intervals')
+)
+def update_cpu_graph(n):
+    # make cpu graphs
+    fetch()
+    global cpu_percent_history, cpu_percent_ma
+    cpu_graphs = []
+    for i in range(len(cpu_percent_history.columns)):
+        cpu_graphs.append(
+            dcc.Graph(
+                id='cpu-{}'.format(i),
+                figure={
+                    'data': [
+                        {'x': cpu_percent_history.index, 'y': cpu_percent_history['cpu-{}'.format(
+                            i)], 'type': 'line', 'name': 'Usage'},
+                        {'x': cpu_percent_ma.index, 'y': cpu_percent_ma['cpu-{}'.format(
+                            i)], 'type': 'line', 'name': 'MA'},
+                    ],
+                    'layout': {
+                        'title': 'CPU-{}'.format(i),
+                        'xaxis': {'title': 'Time'},
+                        'yaxis': {'title': 'percentage'},
+                        'height': 225,
+                        'legend': {'orientation': 'h', 'yanchor': 'top'},
+                        'margin': {'t': 30, 'b': 30, 'r': 30, 'l': 30},
+                    },
+                },
+                style={'display': 'inline-block', 'width': '20%'},
+            ),
+        )
+
+    # create a 3 rows, 5 columns grid from cpu_graphs
+    cpu_grid = [dbc.Row(cpu_graphs[0:5], className='g-0'),
+                dbc.Row(cpu_graphs[5:10], className='g-0'),
+                dbc.Row(cpu_graphs[10:15], className='g-0')]
+
+    return cpu_grid
+
+
+# Define a callback function to update the gauge chart with the CPU frequency
+@app.callback(
+    dash.dependencies.Output('cpu-speed-meter', 'figure'),
+    [dash.dependencies.Input('interval-component', 'n_intervals')]
+)
+def update_gauge_chart(n):
+    # Get the CPU frequency
+    global cpu_freq
+    freq = cpu_freq
+
+    # Create the gauge chart
+    fig = go.Figure(
+        go.Indicator(
+            domain={'x': [0, 1], 'y': [0, 1]},
+            value=freq,
+            mode="gauge+number",
+            title={'text': "CPU Frequency (MHz)"},
+            gauge={
+                'axis': {'range': [0, 4000]},
+                'steps': [
+                    {'range': [0, 1000], 'color': "lightgray"},
+                    {'range': [1000, 2000], 'color': "gray"},
+                    {'range': [2000, 3000], 'color': "lightgreen"},
+                    {'range': [3000, 4000], 'color': "green"}
+                ],
+                'threshold': {
+                    'line': {'color': "red", 'width': 4},
+                    'thickness': 0.75,
+                    'value': 3900
+                }
+            }
+        )
+    )
+
+    return fig
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
