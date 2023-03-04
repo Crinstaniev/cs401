7ce503c0738011027d8b191827d7299db0433674
crinstaniev <zhuangzesen@gmail.com>
2023-03-04 04:39:27 -0500

    finish task2: dashboard

--- project-3/dashboard/app.py
+++ project-3/dashboard/app.py
@@ -13,10 +13,13 @@ r = redis.Redis(host='67.159.94.11', port=6379, db=0)
 cpu_percent_history = None
 cpu_percent_ma = None
 cpu_freq = None
+virtual_memory_used = None
+virtual_memory_free = None
+n_pids = None
 
 
 def fetch():
-    global r, cpu_percent_history, cpu_percent_ma, cpu_freq
+    global r, cpu_percent_history, cpu_percent_ma, cpu_freq, virtual_memory_used, virtual_memory_free, n_pids
     data = fetch_data_from_redis(r)
 
     cpu_percent_history = pd.DataFrame(data['cpu_percent_history'], columns=[
@@ -24,33 +27,28 @@ def fetch():
     cpu_percent_ma = pd.DataFrame(data['cpu_percent_ma'], columns=[
         'cpu-{}'.format(i) for i in range(len(data['cpu_percent_ma'][0]))])
     cpu_freq = data['cpu_freq_current']
+    virtual_memory_used = data['virtual_memory_used'] / 1024 / 1024
+    virtual_memory_free = data['virtual_memory_free'] / 1024 / 1024
+    n_pids = data['n_pids']
 
 
 app.layout = dbc.Container(
     [
-        dcc.Interval(
-            id='interval-component',
-            # interval=5*1000,  # in milliseconds
-        ),
-        dbc.Row(
-            [
-                dbc.Col(
-                    html.H1("Dashboard", className="text-header")
-                )
-            ]
-        ),
-        dbc.Row(
-            [
-                dbc.Col(
-                    html.H3("CPU History", className="text-header")
-                )
-            ]
-        ),
+        dcc.Interval(id='interval-component',),
+        
+        dbc.Row([dbc.Col(html.H1("Dashboard", className="text-header"))]),
+        
+        dbc.Row([dbc.Col(html.H3("CPU History", className="text-header"))]),
         dbc.Row(id='live-update-cpu-component'),
+        
+        dbc.Row([dbc.Col(html.H3("Virtual Memory Usage", className="text-header"))]),
+        dbc.Row(dcc.Graph(id='live-update-memory-component', animate=True)),
+        
         dbc.Row([dbc.Col(html.H3("CPU Frequency", className="text-header"))]),
-        dbc.Row(
-            dcc.Graph(id='cpu-speed-meter', animate=True)
-        )
+        dbc.Row(dcc.Graph(id='cpu-speed-meter', animate=True)),
+        
+        dbc.Row([dbc.Col(html.H3("Number of Processes", className="text-header"))]),
+        dbc.Row(dcc.Graph(id='live-update-n-pids-component', animate=True))
     ],
     fluid=True,
 )
@@ -122,11 +120,87 @@ def update_gauge_chart(n):
                     {'range': [2000, 3000], 'color': "lightgreen"},
                     {'range': [3000, 4000], 'color': "green"}
                 ],
-                'threshold': {
-                    'line': {'color': "red", 'width': 4},
-                    'thickness': 0.75,
-                    'value': 3900
-                }
+                'bar': {'color': "red"},
+            }
+        )
+    )
+
+    return fig
+
+
+@app.callback(
+    Output('live-update-memory-component', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+def update_virtual_memory_chart(n):
+    # Get the virtual memory usage
+    global virtual_memory_used, virtual_memory_free
+    # Create the stacked bar chart
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+        y=['Memory Usage'],
+        x=[virtual_memory_used],
+        name='Used Memory',
+        orientation='h',
+        width=0.5,
+        marker=dict(color='rgb(204,204,204)')
+    ))
+    fig.add_trace(go.Bar(
+        y=['Memory Usage'],
+        x=[virtual_memory_free],
+        name='Free Memory',
+        orientation='h',
+        width=0.5,
+        marker=dict(color='rgb(158,202,225)')
+    ))
+
+    # Customize the layout of the chart
+    fig.update_layout(
+        title='Virtual Memory Usage',
+        xaxis=dict(
+            title='Memory (MB)',
+            titlefont_size=16,
+            tickfont_size=14,
+        ),
+        yaxis_tickfont_size=14,
+        legend=dict(
+            x=0,
+            y=1.0,
+            bgcolor='rgba(255, 255, 255, 0)',
+            bordercolor='rgba(255, 255, 255, 0)'
+        ),
+        barmode='stack',
+        bargap=0.15,  # gap between bars of adjacent location coordinates.
+        bargroupgap=0.1  # gap between bars of the same location coordinate.
+    )
+
+    return fig
+
+
+@app.callback(
+    Output('live-update-n-pids-component', 'figure'),
+    Input('interval-component', 'n_intervals')
+)
+def update_n_pids(n):
+    # Get the CPU frequency
+    global n_pids
+
+    # Create the gauge chart
+    fig = go.Figure(
+        go.Indicator(
+            domain={'x': [0, 1], 'y': [0, 1]},
+            value=n_pids,
+            mode="gauge+number",
+            title={'text': "Number of Processes"},
+            gauge={
+                'axis': {'range': [0, 4000]},
+                'steps': [
+                    {'range': [0, 1000], 'color': "lightgray"},
+                    {'range': [1000, 2000], 'color': "gray"},
+                    {'range': [2000, 3000], 'color': "lightgreen"},
+                    {'range': [3000, 4000], 'color': "green"}
+                ],
+                'bar': {'color': "red"},
             }
         )
     )
